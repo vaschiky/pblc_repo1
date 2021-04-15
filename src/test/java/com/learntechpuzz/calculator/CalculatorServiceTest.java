b1a9f3a2871f3e940bb63ead3b4abefc5626326f
vaschiky <79928315+vaschiky@users.noreply.github.com>
2021-04-15 12:00:34 +0530

    changing to fail

--- src/test/java/com/learntechpuzz/calculator/CalculatorServiceTest.java
+++ src/test/java/com/learntechpuzz/calculator/CalculatorServiceTest.java
@@ -15,7 +15,7 @@ class CalculatorServiceTest {
 
 	@Test
 	public void testAdd() {
-		assertThat(calculatorService.add(1, 2)).isEqualTo(3);
+		assertThat(calculatorService.add(1, 2)).isEqualTo(4);
 	}
 
 }
